4da7ff87905d66d1e22ebf5400e25fdac9665f7c
melek15 <malek15romdhan@gmail.com>
2022-08-03 22:10:47 +0200

    first commit

---
+++ test/Readme.txt
@@ -0,0 +1,6 @@
+The data in  ADL_proc_AG and FALL_proc_AG files are organized as follows:
++─────+─────+─────+─────+─────+─────+──────────+───────────+
+| ax  , ay  , az  , gx  , gy  , gz  , SVM Acc  , SVM Gyro  |
++─────+─────+─────+─────+─────+─────+──────────+───────────+
+where (ax,ay,az) are the tri-acceleration components of the acceleration sensor ADXL345.
+and (gx,gy,gz) are the components of the gyro sensor ITG3200.
---
+++ test/main.py
@@ -0,0 +1,76 @@
+import numpy as np
+import tensorflow
+from tensorflow.keras.layers import Conv1D, BatchNormalization, MaxPooling1D, Dropout, Flatten, Dense
+
+x=[]
+y=[]
+file_ADL='ADL_CNN.txt' # contains all the ADLs data windows (length of the window 401*3)
+file_FALL='FALL_CNN.txt' # contains all the Falls data windows (length of the window 401*3)
+data_ADL = np.loadtxt(file_ADL, dtype=float, delimiter=',')
+data_FALL = np.loadtxt(file_FALL, dtype=float, delimiter=',')
+for i in range(len(data_FALL),len(data_ADL)):
+        x.append(data_ADL[i])
+        y.append(0)
+for i in range(len(data_FALL)):
+    x.append(data_ADL[i])
+    y.append(0)
+    x.append(data_FALL[i])
+    y.append(1)
+
+
+#%%
+
+labels=['ADL','FALL'] # labels : ADL=0; Fall=1
+
+#%%
+
+X= np.array(x).reshape(len(x), 401, 3)
+Y = np.array(y)
+
+print(X.shape)
+print(Y.shape)
+
+from sklearn.model_selection import train_test_split
+#split the data to 60% training, 20% test and 20% validation
+X_train, X_t, y_train, y_t = train_test_split(X, Y, test_size = 0.4, random_state =42, stratify = Y)
+X_test, X_val, y_test, y_val = train_test_split(X_t, y_t, test_size=0.5, random_state=42)
+
+print(X_train.shape)
+print(X_test.shape)
+print(X_val.shape)
+print(y_test.shape)
+
+model = tensorflow.keras.Sequential()
+#1st convolution layer
+model.add(Conv1D(16, 3,activation='relu',padding='same',input_shape=(401,3)))
+model.add(BatchNormalization())
+model.add(MaxPooling1D(pool_size=3,padding='same'))
+
+#2nd convolution layer
+model.add(Conv1D(32, 3,activation='relu',padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling1D(pool_size=3,padding='same'))
+
+#3rd convolution layer
+model.add(Conv1D(64,3,activation='relu',padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling1D(pool_size=3,padding='same'))
+
+
+#4th convolution layer
+model.add(Conv1D(128,3,activation='relu',padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling1D(pool_size=3,padding='same'))
+model.add(Dropout(0.5))
+
+#fully connected layer
+model.add(Flatten())
+model.add(Dense(64, activation='relu', kernel_regularizer=tensorflow.keras.regularizers.l2(0.001)))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+model.add(Dense(2,activation='softmax'))
+
+
+#%%
+
+print(model.summary())
